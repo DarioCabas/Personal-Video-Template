1099179c5173786d694a1b6f3fceeb94b445db8a
Dario <hz-hertzio@hotmail.com>
2020-10-15 17:15:14 -0500

    cards

--- mypersvid/src/App.js
+++ mypersvid/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Title from './Components/Title'
-import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
 import './App.css'
+
+
 
 const App = () =>{
 
@@ -10,17 +13,20 @@ const App = () =>{
         <div>
         <Title></Title>
 
-        <Card>
-            <Button>Subir</Button>
-        </Card>
-        
-        <Card>
-            <Button>Generar Videos</Button>
-        </Card>
 
-
+        <Grid item xs={12}>
+        <Grid container justify="center" spacing='10' >
+          {[0, 1].map((value) => (
+            <Grid key={value} item>
+                     <Card>
+                        <Button variant="contained" color="primary">Generar Videos</Button>
+                     </Card>
+            </Grid>
+          ))}
+        </Grid>
+        </Grid>
       
-            <Button size="small" color="blue">Hola</Button>
+           <Button variant="outlined" color="primary" >Hola</Button>
     
 
         </div>
