d60dcd938611fef419c58c4e97350b1904df44e8
Dario <hz-hertzio@hotmail.com>
2020-10-16 16:53:43 -0500

    card component

--- mypersvid/src/App.js
+++ mypersvid/src/App.js
@@ -1,28 +1,37 @@
 import React from 'react'
 import Title from './Components/Title'
-import Card from './Components/Card'
+import Cart from './Components/Card'
 import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css'
 
 
+const useStyles = makeStyles({
+    gridContainer:{
+    paddingLeft: '80px',
+    paddingRight:'80px'
+    }
+})
+
 const App = () =>{
 
+const classes = useStyles();
     return (
         <div>
 
         <Title title='VIDEOS PERSONALIZADOS'></Title>
-        <Grid container justify="center" spacing={5}>
+        <Grid container justify="center" spacing={5} className={classes.gridContainer}>
 
-            <Grid item xs={10} sm={6} md={4} >
-                <Card></Card>
+            <Grid item xs={6} sm={6} md={4} >
+                <Cart sentence1="Carga el archivo con la lista de tus amigos" sentence2="Busca el archivo" button1="Cargar CSV"></Cart>
             </Grid>
             
-            <Grid item xs = {10} sm = {6} md = {4} >
-                <Card></Card>
+            <Grid item xs={6} sm={6} md={4} >
+                <Cart sentence1="Ahora, genera los videos para cada uno de ellos" sentence2="Dale click en genera" button1="Generar"></Cart>
             </Grid>
 
-            <Grid item xs = {10} sm = {6} md = {4} >
-                <Card></Card>
+            <Grid item xs={6} sm={6} md={4} >
+                <Cart sentence1="Ahora haz que todos en la lista de tus amigos lo vean" sentence2="Enviar los videos" button1="Enviar"></Cart>
             </Grid>
 
         </Grid>
--- mypersvid/src/Components/Card.js
+++ mypersvid/src/Components/Card.js
@@ -8,7 +8,7 @@ import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
-    minWidth: 275,
+    minWidth: 200,
   },
   bullet: {
     display: 'inline-block',
@@ -23,31 +23,31 @@ const useStyles = makeStyles({
   },
 });
 
-export default function OutlinedCard() {
+const Cart = ({sentence1, sentence2, button1}) => {
+
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+
 
   return (
-    <Card className={classes.root} variant="outlined">
+
+    <Card className={classes.root} variant="outlined" >
       <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
+
         <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
+          {sentence1}
         </Typography>
         <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+        <br></br>
+          {sentence2}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <Button size="small" >{button1}</Button>
       </CardActions>
     </Card>
   );
-}
+
+
+  }
+
+export default Cart
