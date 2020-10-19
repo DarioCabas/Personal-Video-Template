2c973243128069de1d76697afe3090184f11770e
unknown <cristhian1995cobos@hotmail.com>
2020-10-19 11:23:34 -0500

    add functionlity

--- mypersvid/src/App.js
+++ mypersvid/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Title from './Components/Title'
 import Cart from './Components/Card'
 import Grid from '@material-ui/core/Grid'
@@ -6,6 +6,7 @@ import axios from 'axios'
 import { makeStyles } from '@material-ui/core/styles'
 import './App.css'
 import CircularProgress from '@material-ui/core/CircularProgress';
+
 
 
 
@@ -26,11 +27,7 @@ const useStyles = makeStyles((theme) => ({
 ));
 
 
-const send = () => {
 
-  console.log('HOLA TODO BIEN 3');
-
-}
 
 
 const App = () => {
@@ -41,23 +38,80 @@ const App = () => {
   const [selectedFile, setSelectedFile] = useState();
   const [message, setMessage] = useState();
   const [message2, setMessage2] = useState();
+  const [message3, setMessage3] = useState();
   const [activeCharge, setActiveCharge] = useState(false);
   const [activeGenerate, setActiveGenerate] = useState(true);
   const [activeSend, setActiveSend] = useState(true);
   const [error, setError] = useState(0);
   const [charging, setCharging] = useState(0);
+  const [listening, setListening] = useState(0);
+  const [temp, setTemp] = useState(0);
+  const [pkCampaign, setPkCampaign ] = useState();
+
+
+  useEffect(() => {
+    setInterval(() => {
+      setTemp((prevTemp) => prevTemp + 1)
+    }, 20000);
+  }, [])
+
+  useEffect(() => {
+    if (listening === 1) {
+      console.log('listening');
+      let url = "http://test-backend-dev2.us-east-2.elasticbeanstalk.com/api/create/";
+
+      axios({
+        method: 'GET',
+        url: url,
+      }
+      ).then(//function(response)
+
+        response => {
+          const estado = response.data
+          setMessage2(response.data.message)
+          console.log(response.status);
+          console.log(response.data.message);
+          if (estado.status === 200) {
+
+            setListening(0);
+            setActiveSend(false);
+            setActiveCharge(true);
+
+          }
+
+          console.log(estado)
+        }
+        //  console.log(response)
+
+
+      ).catch(function (response) {
+
+        console.log(response)
+
+      });
+
+    }
+  }, [temp])
 
   const chargingbar = (event) => {
     if (charging === 1) {
       return (
         <div className={classes.root}>
           <div  >
-            <CircularProgress  />
+            <CircularProgress />
 
           </div>
           <div>
-            <p style={{color:"blue"}}
+            <p style={{ color: error === 0 ? 'blue' : 'red' }}
+            >{message}</p>
+          </div>
+          <div>
+            <p style={{ color: error === 0 ? 'blue' : 'red' }}
             >{message2}</p>
+          </div>
+          <div>
+            <p style={{ color: error === 0 ? 'blue' : 'red'}}
+            >{message3}</p>
           </div>
         </div>
       )
@@ -94,11 +148,12 @@ const App = () => {
       response => {
         const estado = response.data
         const pk = response.data.data.pk
+        setPkCampaign(pk)
         setMessage(response.data.message)
-    
+
         console.log(estado)
         if (estado.status === 200) {
-          console.log('este es el : '+ pk)
+          console.log('este es el : ' + pk)
           generate(pk);
           setError(0);
         } else {
@@ -115,8 +170,44 @@ const App = () => {
     });
   }
 
+  const sendemail = () => {
+    
+    let url = ("http://test-backend-dev2.us-east-2.elasticbeanstalk.com/api/send/"+pkCampaign);
+
+    axios({
+      method: 'GET',
+      url: url,
+    }
+    ).then(//function(response)
+
+      response => {
+        const estado = response.data
+        setMessage3(response.data.message)
+        console.log(response.status);
+        console.log(response.data.message);
+        if (estado.status === 200) {
+          setCharging(0);
+          setActiveCharge(false);
+          setActiveSend(false);
+          setActiveGenerate(false);
+
+          console.log('finalice')
+        }
+
+        
+      }
+      //  console.log(response)
+
+
+    ).catch(function (response) {
+
+      console.log(response)
+
+    });
+  }
+
   const generate = e => {
-console.log(e)
+    console.log(e)
     const data = new FormData()
     data.append('pk', e)
     console.warn(e);
@@ -134,9 +225,10 @@ console.log(e)
 
       response => {
         const estado = response.data
-        setMessage(response.data.message)
+        setMessage2(response.data.message)
         if (estado.status === 200) {
           setError(0);
+          setListening(1);
         }
         else {
           setError(1);
@@ -169,14 +261,9 @@ console.log(e)
         </Grid>
 
         <Grid item xs={6} sm={6} md={4} >
-          <Cart sentence1="Listo, enviemoslo a todos tus amigos" sentence2="Enviar los videos" button1="Enviar" onClick={send} modo={0} enable={activeSend}></Cart>
+          <Cart sentence1="Listo, enviemoslo a todos tus amigos" sentence2="Enviar los videos" button1="Enviar" onClick={sendemail} modo={0} enable={activeSend}></Cart>
         </Grid>
-        <Grid container justify="center" item xs={12} >
-          <div>
-            <p style={{ color: error === 0 ? 'blue' : 'red' }}
-            >{message}</p>
-          </div>
-        </Grid>
+        
         <Grid container justify="center" item xs={12}  >
           <div>
             {chargingbar()}
